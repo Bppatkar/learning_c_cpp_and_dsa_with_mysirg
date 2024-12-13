efe89d863a229adc84b1decf34eda2ee6e6e8198
Bppatkar <bhanupratappatkar777@gmail.com>
2024-12-13 15:42:11 +0530

    New Update with 8 assignment question

--- c programs/assignments/assignment7/print.c
+++ c programs/assignments/assignment7/print.c
@@ -106,13 +106,13 @@ int main()
   return 0;
 } */
 
-/* // 10. Write a program to print a table of 5.
+/*
 #include <stdio.h>
 int main()
 {
   for (int i = 1; i <= 10; i++)
   {
-    printf("%d\n", 5 * i);
+    printf(" 5 * %d = %d\n", i, 5 * i);
   }
   return 0;
 } */
